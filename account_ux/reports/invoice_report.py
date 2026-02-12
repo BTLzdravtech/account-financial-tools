236d36475b8bc7230e43ac37c18257ee0bab5871
fcarlini <felipercarlini@gmail.com>
2026-02-12 16:22:54 -0300

    [ADD] requested changes

--- account_ux/reports/invoice_report.py
+++ account_ux/reports/invoice_report.py
@@ -13,18 +13,18 @@ class AccountInvoiceReport(models.Model):
         store=False,
     )
     # agregamos widgets monetary, referencia a company currency en string y help
-    price_subtotal = fields.Monetary(
+    company_price_subtotal = fields.Monetary(
         currency_field="current_currency_id",
         string="Untaxed Total (CC)",
         help="Untaxed Total in the company's currency where it is set",
     )
-    price_total = fields.Monetary(string="Total", currency_field="currency_id")
-    price_average = fields.Monetary(
+    company_price_total = fields.Monetary(string="Total", currency_field="currency_id")
+    company_price_average = fields.Monetary(
         currency_field="current_currency_id",
         string="Average Price (CC)",
         help="Average Price in the company's currency where it is set",
     )
-    price_margin = fields.Float(string="Margin (CC)", help="Margin in the company's currency where it is set")
+    company_price_margin = fields.Float(string="Margin (CC)", help="Margin in the company's currency where it is set")
     # creamos nuevos campos para tener descuentos, vinculos e importes en moneda de compañía
     total_cc = fields.Monetary(
         string="Total (CC)",
@@ -33,7 +33,7 @@ class AccountInvoiceReport(models.Model):
         currency_field="current_currency_id",
     )
     line_id = fields.Many2one("account.move.line", string="Journal Item", readonly=True)
-    price_subtotal_currency = fields.Monetary(string="Untaxed Amount in Currency", currency_field="currency_id")
+    company_price_subtotal_currency = fields.Monetary(string="Untaxed Amount in Currency", currency_field="currency_id")
     price_unit = fields.Monetary(
         "Unit Price",
         readonly=True,
